f54d1e78c65a0c6911c292fdc7abbcee29105912
Tony Beltramelli <tbeltramelli@gmail.com>
2014-04-15 17:04:46 +0200

    update Android app behavior

--- AndroidTaco/src/dk/itu/smdp/activity/SurveyActivity.java
+++ AndroidTaco/src/dk/itu/smdp/activity/SurveyActivity.java
@@ -15,72 +15,73 @@ import dk.itu.smdp.model.question.RatingQuestion;
 /**
  * Created by centos on 4/13/14.
  */
-public class SurveyActivity extends AbtractActivity implements QuestionContainable {
-
-    private int _currentCategory = 0;
-    private int _currentPage = 0;
-    private int _mandatoryQuestionsNumber = 0;
-    private View _nextButton;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.page);
-
-        TextView categoryTitle = (TextView) this.findViewById(R.id.category_title_text_view);
-        categoryTitle.setText(_survey.getCategories().get(0).get_title());
-
-        displayPage();
-
-    }
-
-    private void displayPage()
-    {
-    	Page page = _survey.getCategories().get(_currentCategory).get_pages().get(_currentPage);
-    	
-    	LinearLayout parent = (LinearLayout) this.findViewById(R.id.questions_linearlayout);
-
-        for(Question q : page.getQuestions() )
-        {
-        	q.setContainer(this);
-            View questionView = q.getView(this , parent);
-            parent.addView(questionView);
-            
-            if(q.isMandatory() && !(q instanceof RatingQuestion) && !(q instanceof RankingQuestion))
-            {
-            	_mandatoryQuestionsNumber ++;
-            }
-        }
-        
-        _nextButton = findViewById(R.id.page_screen_next_button);
-        _nextButton.setVisibility(View.INVISIBLE);
-    }
-
+public class SurveyActivity extends AbtractActivity implements QuestionContainable
+{
+	
+	private int _currentCategory = 0;
+	private int _currentPage = 0;
+	private int _mandatoryQuestionsNumber = 0;
+	private View _nextButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.page);
+		
+		TextView categoryTitle = (TextView) this.findViewById(R.id.category_title_text_view);
+		categoryTitle.setText(_survey.getCategories().get(0).get_title());
+		
+		displayPage();
+		
+	}
+	
+	private void displayPage()
+	{
+		Page page = _survey.getCategories().get(_currentCategory).get_pages().get(_currentPage);
+		
+		LinearLayout parent = (LinearLayout) this.findViewById(R.id.questions_linearlayout);
+		
+		for (Question q : page.getQuestions())
+		{
+			q.setContainer(this);
+			View questionView = q.getView(this, parent);
+			parent.addView(questionView);
+			
+			if (q.isMandatory() && !(q instanceof RatingQuestion) && !(q instanceof RankingQuestion))
+			{
+				_mandatoryQuestionsNumber++;
+			}
+		}
+		
+		_nextButton = findViewById(R.id.page_screen_next_button);
+		_nextButton.setVisibility(View.INVISIBLE);
+	}
+	
 	@Override
 	public void updateQuestionAnswer(Question question)
 	{
-		if(question.isMandatory() && question.isQuestionAnswered())
+		int answeredQuestions = 0;
+		
+		if (question.isMandatory() && question.isQuestionAnswered())
 		{
-			int answeredQuestions = 0;
-			
 			Page page = _survey.getCategories().get(_currentCategory).get_pages().get(_currentPage);
-	    	
-			for(Question q : page.getQuestions() )
-	        {
-				if(q.isMandatory() && q.isQuestionAnswered())
-				{
-					answeredQuestions ++;
-				}
-	        }
 			
-			Log.wtf("update question answer", answeredQuestions + " / " + _mandatoryQuestionsNumber);
-			
-			if(answeredQuestions == _mandatoryQuestionsNumber)
+			for (Question q : page.getQuestions())
 			{
-				_nextButton.setVisibility(View.VISIBLE);
-			}else{
-				_nextButton.setVisibility(View.INVISIBLE);
-			}
+				if (q.isMandatory() && q.isQuestionAnswered())
+				{
+					answeredQuestions++;
+				}
+			}	
+		}
+		
+		if (answeredQuestions == _mandatoryQuestionsNumber)
+		{
+			_nextButton.setVisibility(View.VISIBLE);
+		} else
+		{
+			_nextButton.setVisibility(View.INVISIBLE);
 		}
 	}
 }
