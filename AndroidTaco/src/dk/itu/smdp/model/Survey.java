6b152648d0b3e4a1968a10d1ff9498331baabe55
Tony Beltramelli <tbeltramelli@gmail.com>
2014-05-13 14:48:48 +0200

    update instances

--- AndroidTaco/src/dk/itu/smdp/model/Survey.java
+++ AndroidTaco/src/dk/itu/smdp/model/Survey.java
@@ -98,80 +98,74 @@ public class Survey
 	
 	private void init()
 	{
-		_title = "Woooooaaaahh";
-		_description = "That's cool";
+		_title = "Survey example";
+		_description = "Survey example instance";
 		_isAnonymous = false;
-		_date = "02/05/2014";
-		_email = "tonb@itu.dk";
+		_date = "30/04/2014";
+		_email = "deebay@jubii.dk";
 		
 		Person p = new Person();
 		
 		PersonAttribute personAttribute1 = new PersonAttribute("name");
 		p.addAttribute(personAttribute1);
+		PersonAttribute personAttribute2 = new PersonAttribute("age");
+		p.addAttribute(personAttribute2);
+		PersonAttribute personAttribute3 = new PersonAttribute("address");
+		p.addAttribute(personAttribute3);
 		
 		this.setPerson(p);
 		
 		
 		
-		Category category1 = new Category("what's up", "");
+		Category category1 = new Category("IT University of Copenhagen", "The topic of this section is about IT University of Copenhagen");
 		
 		
 		Page page1 = new Page();
 		
+		Question question1 = QuestionFactory.create(Question.OPEN_FIELD, false, "What do you thing about IT University of Copenhagen?");								            
 		
-		Question question1 = QuestionFactory.create(Question.RANKING, true, "Rank your hangover state today");
-		
-		question1.addAnswer(AnswerFactory.create(Answer.RANKING, "soooo bad"));		
-		question1.addAnswer(AnswerFactory.create(Answer.RANKING, "comme si, comme ca"));		
-		question1.addAnswer(AnswerFactory.create(Answer.RANKING, "just need another beer"));		
-		question1.addAnswer(AnswerFactory.create(Answer.RANKING, "just get me a coffe"));		
-		question1.addAnswer(AnswerFactory.create(Answer.RANKING, "did I really brought that girl home"));		
 			
 		page1.addQuestion(question1);
 		
 			
-		Question question2 = QuestionFactory.create(Question.RATING, false, "Rate how much you want to swim today", 0, 1000, 100);
+		Question question2 = QuestionFactory.create(Question.MULTIPLE_CHOICE, false, "How did you know about ITU?", 2, 3);
+			
+		question2.addAnswer(AnswerFactory.create(Answer.BINARY, "University"));
+		question2.addAnswer(AnswerFactory.create(Answer.BINARY, "Internet"));
+		question2.addAnswer(AnswerFactory.create(Answer.BINARY, "Friends"));
+		question2.addAnswer(AnswerFactory.create(Answer.USER_INPUT, "Other"));
 		page1.addQuestion(question2);
+		
+			
+		Question question3 = QuestionFactory.create(Question.RATING, true, "How much do you like the canteen's coffee?", 0, 10, 1);
+		page1.addQuestion(question3);
 		
 		category1.addPage(page1);
 		
 		Page page2 = new Page();
 		
-		Question question3 = QuestionFactory.create(Question.OPEN_FIELD, false, "What's up ?");								            
 		
 			
-		page2.addQuestion(question3);
-		
+		Question question4 = QuestionFactory.create(Question.MUTUALLY_EXCLUSIVE, true, "Select your favorite ITU course");
 			
-		Question question4 = QuestionFactory.create(Question.MULTIPLE_CHOICE, false, "What would you like to drink ?", 3, 5);
-			
-		Answer answer0 = AnswerFactory.create(Answer.BINARY, "beer");
+		Answer answer0 = AnswerFactory.create(Answer.BINARY, "SPCT");
 		question4.addAnswer(answer0);
 		
 		
 			
-		Question question4_0 = QuestionFactory.create(Question.MUTUALLY_EXCLUSIVE, true, "Which brand ?");
+		Question question4_0 = QuestionFactory.create(Question.MULTIPLE_CHOICE, false, "Which platforms are you interested in?", 1, 2);
 			
-		question4_0.addAnswer(AnswerFactory.create(Answer.BINARY, "Tuborg"));
-		question4_0.addAnswer(AnswerFactory.create(Answer.BINARY, "Heineken"));
+		Answer answer1 = AnswerFactory.create(Answer.BINARY, "Android");
+		question4_0.addAnswer(answer1);
+		
+		
+			
+		Question question4_0_1 = QuestionFactory.create(Question.RATING, false, "Rate lectures Android content?", 0, 100, 10);
+		answer1.addSubQuestion(question4_0_1);
+		question4_0.addAnswer(AnswerFactory.create(Answer.BINARY, "iPhone"));
+		question4_0.addAnswer(AnswerFactory.create(Answer.BINARY, "Windows Phone"));
 		answer0.addSubQuestion(question4_0);
-		Answer answer1 = AnswerFactory.create(Answer.BINARY, "wine");
-		question4.addAnswer(answer1);
-		
-		
-			
-		Question question4_1 = QuestionFactory.create(Question.MUTUALLY_EXCLUSIVE, true, "Which wine ?");
-			
-		question4_1.addAnswer(AnswerFactory.create(Answer.BINARY, "cote du rhone"));
-		question4_1.addAnswer(AnswerFactory.create(Answer.BINARY, "bordeau"));
-		question4_1.addAnswer(AnswerFactory.create(Answer.BINARY, "savagnin"));
-		question4_1.addAnswer(AnswerFactory.create(Answer.BINARY, "merlot"));
-		question4_1.addAnswer(AnswerFactory.create(Answer.BINARY, "tempramillo"));
-		answer1.addSubQuestion(question4_1);
-		question4.addAnswer(AnswerFactory.create(Answer.BINARY, "water"));
-		question4.addAnswer(AnswerFactory.create(Answer.BINARY, "juice"));
-		question4.addAnswer(AnswerFactory.create(Answer.BINARY, "whisky"));
-		question4.addAnswer(AnswerFactory.create(Answer.BINARY, "gin"));
+		question4.addAnswer(AnswerFactory.create(Answer.BINARY, "SAD1"));
 		page2.addQuestion(question4);
 		
 		category1.addPage(page2);
@@ -180,60 +174,26 @@ public class Survey
 		
 		
 		
-		Category category2 = new Category("who is that chicks", "she is cute");
+		Category category2 = new Category("Professional information?", "The topic is about your professional carrier");
 		
 		
 		Page page3 = new Page();
 		
 		
 			
-		Question question5 = QuestionFactory.create(Question.YES_NO, false, "Is she single ?");
+		Question question5 = QuestionFactory.create(Question.YES_NO, false, "Are you currently working?");
 		page3.addQuestion(question5);
 		
-			
-		Question question6 = QuestionFactory.create(Question.MUTUALLY_EXCLUSIVE, true, "Where do you wanna bring her ?");
-			
-		Answer answer2 = AnswerFactory.create(Answer.BINARY, "restaurant");
-		question6.addAnswer(answer2);
+		Question question6 = QuestionFactory.create(Question.RANKING, false, "Rate the next companies according to your preferences");
 		
-		
+		question6.addAnswer(AnswerFactory.create(Answer.RANKING, "Microsoft"));		
+		question6.addAnswer(AnswerFactory.create(Answer.RANKING, "Apple"));		
+		question6.addAnswer(AnswerFactory.create(Answer.RANKING, "Google"));		
+		question6.addAnswer(AnswerFactory.create(Answer.RANKING, "Amazon"));		
 			
-		Question question6_2 = QuestionFactory.create(Question.MUTUALLY_EXCLUSIVE, false, "which kind of food ?");
-			
-		Answer answer3 = AnswerFactory.create(Answer.BINARY, "asian");
-		question6_2.addAnswer(answer3);
-		
-		
-			
-		Question question6_2_3 = QuestionFactory.create(Question.MUTUALLY_EXCLUSIVE, true, "which country");
-			
-		question6_2_3.addAnswer(AnswerFactory.create(Answer.BINARY, "japan"));
-		question6_2_3.addAnswer(AnswerFactory.create(Answer.BINARY, "chinese"));
-		question6_2_3.addAnswer(AnswerFactory.create(Answer.BINARY, "thai"));
-		answer3.addSubQuestion(question6_2_3);
-		question6_2.addAnswer(AnswerFactory.create(Answer.BINARY, "danish"));
-		question6_2.addAnswer(AnswerFactory.create(Answer.BINARY, "fucking fast food"));
-		answer2.addSubQuestion(question6_2);
-		question6.addAnswer(AnswerFactory.create(Answer.BINARY, "bar"));
 		page3.addQuestion(question6);
 		
 		category2.addPage(page3);
-		
-		Page page4 = new Page();
-		
-		
-			
-		Question question7 = QuestionFactory.create(Question.MUTUALLY_EXCLUSIVE, true, "Is taco the best language in the world ?");
-			
-		question7.addAnswer(AnswerFactory.create(Answer.BINARY, "yes"));
-		question7.addAnswer(AnswerFactory.create(Answer.BINARY, "nope"));
-		question7.addAnswer(AnswerFactory.create(Answer.BINARY, "fuck it"));
-		question7.addAnswer(AnswerFactory.create(Answer.BINARY, "maybe"));
-		question7.addAnswer(AnswerFactory.create(Answer.BINARY, "I don't care"));
-		question7.addAnswer(AnswerFactory.create(Answer.USER_INPUT, "kind of"));
-		page4.addQuestion(question7);
-		
-		category2.addPage(page4);
 		
 		this.addCategory(category2);
 		
